e924dfd659e773d33d146eb8ad21554ef47ca9ef
Gilbert Torchon <infotorch2014@gmail.com>
2020-03-13 16:26:07 -0400

    Fix linting and prettier errors in App

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
-import * as React from "react";
-import { Platform, StatusBar, StyleSheet, View } from "react-native";
-import { SplashScreen } from "expo";
-import * as Font from "expo-font";
-import { Ionicons } from "@expo/vector-icons";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import * as React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { SplashScreen } from 'expo';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import BottomTabNavigator from "./navigation/BottomTabNavigator";
-import useLinking from "./navigation/useLinking";
-import SignupScreen from "./screens/SignUpScreen";
-import LoginScreen from "./screens/LoginScreen";
-import ResetPasswordScreen from "./screens/ResetPasswordScreen";
-import ResetPasswordTwoScreen from "./screens/ResetPasswordTwoScreen";
+import PropTypes from 'prop-types';
+import BottomTabNavigator from './navigation/BottomTabNavigator';
+import useLinking from './navigation/useLinking';
+import SignupScreen from './screens/SignUpScreen';
+import LoginScreen from './screens/LoginScreen';
+import ResetPasswordScreen from './screens/ResetPasswordScreen';
+import ResetPasswordTwoScreen from './screens/ResetPasswordTwoScreen';
+import SpaceMono from './assets/fonts/SpaceMono-Regular.ttf';
+import RobotoBlack from './assets/fonts/Roboto-Black.ttf';
+import RobotoBold from './assets/fonts/Roboto-Bold.ttf';
+import RobotoBoldItalic from './assets/fonts/Roboto-BoldItalic.ttf';
+import RobotoItalic from './assets/fonts/Roboto-Italic.ttf';
+import RobotoLight from './assets/fonts/Roboto-Light.ttf';
+import RobotoLightItalic from './assets/fonts/Roboto-LightItalic.ttf';
+import RobotoMedium from './assets/fonts/Roboto-Medium.ttf';
+import RobotoMediumItalic from './assets/fonts/Roboto-MediumItalic.ttf';
+import RobotoRegular from './assets/fonts/Roboto-Regular.ttf';
+import RobotoThin from './assets/fonts/Roboto-Thin.ttf';
+import RobotoThinItalic from './assets/fonts/Roboto-ThinItalic.ttf';
 
 const Stack = createStackNavigator();
 
@@ -33,18 +46,18 @@ export default function App(props) {
         // Load fonts
         await Font.loadAsync({
           ...Ionicons.font,
-          "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf"),
-          "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
-          "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
-          "Roboto-BoldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
-          "Roboto-Italic": require("./assets/fonts/Roboto-Italic.ttf"),
-          "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
-          "Roboto-LightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
-          "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
-          "Roboto-MediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
-          "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-          "Roboto-Thin": require("./assets/fonts/Roboto-Thin.ttf"),
-          "Roboto-ThinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf")
+          'space-mono': SpaceMono,
+          'Roboto-Black': RobotoBlack,
+          'Roboto-Bold': RobotoBold,
+          'Roboto-BoldItalic': RobotoBoldItalic,
+          'Roboto-Italic': RobotoItalic,
+          'Roboto-Light': RobotoLight,
+          'Roboto-LightItalic': RobotoLightItalic,
+          'Roboto-Medium': RobotoMedium,
+          'Roboto-MediumItalic': RobotoMediumItalic,
+          'Roboto-Regular': RobotoRegular,
+          'Roboto-Thin': RobotoThin,
+          'Roboto-ThinItalic': RobotoThinItalic
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
@@ -60,46 +73,38 @@ export default function App(props) {
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return null;
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
-        <NavigationContainer
-          ref={containerRef}
-          initialState={initialNavigationState}
-        >
-          <Stack.Navigator>
-            {/* <Stack.Screen name="Root" component={BottomTabNavigator} /> */}
-            <Stack.Screen
-              options={{ headerShown: false }}
-              name="Root"
-              component={SignupScreen}
-            />
-            <Stack.Screen
-              options={{ headerShown: false }}
-              name="Login"
-              component={LoginScreen}
-            />
-            <Stack.Screen
-              options={{ headerShown: false }}
-              name="ResetPassword"
-              component={ResetPasswordScreen}
-            />
-            <Stack.Screen
-              options={{ headerShown: false }}
-              name="ResetPasswordTwoScreen"
-              component={ResetPasswordTwoScreen}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
-    );
   }
+  return (
+    <View style={styles.container}>
+      {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+      <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
+        <Stack.Navigator>
+          <Stack.Screen name="Root" component={BottomTabNavigator} />
+          <Stack.Screen options={{ headerShown: false }} name="Root" component={SignupScreen} />
+          <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="ResetPassword"
+            component={ResetPasswordScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="ResetPasswordTwoScreen"
+            component={ResetPasswordTwoScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
+  );
 }
+
+App.propTypes = {
+  skipLoadingScreen: PropTypes.bool.isRequired
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: '#fff'
   }
 });
