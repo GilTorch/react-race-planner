f05fb2da40aef4c0dead32fe9b1711bfbef3bd19
Nick Rameau <r@meau.me>
2020-03-17 23:28:50 -0400

    Introduce Main Stack

---
+++ navigation/MainStack.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import BottomTab from './BottomTab';
+
+const Stack = createStackNavigator();
+
+export default function MainStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Main" component={BottomTab} />
+    </Stack.Navigator>
+  );
+}
