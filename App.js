aa216caa5f46c1b8ec860cd7cc50454e44894739
Nick Rameau <r@meau.me>
2020-03-24 12:37:37 -0400

    Use AppLoading for asset preloading

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { SplashScreen } from 'expo';
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, View, Image } from 'react-native';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
-
 import PropTypes from 'prop-types';
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
+
 import useLinking from './navigation/useLinking';
 import SpaceMono from './assets/fonts/SpaceMono-Regular.ttf';
 import RobotoBlack from './assets/fonts/Roboto-Black.ttf';
+import RobotoBlackItalic from './assets/fonts/Roboto-BlackItalic.ttf';
 import RobotoBold from './assets/fonts/Roboto-Bold.ttf';
 import RobotoBoldItalic from './assets/fonts/Roboto-BoldItalic.ttf';
 import RobotoItalic from './assets/fonts/Roboto-Italic.ttf';
@@ -19,57 +21,71 @@ import RobotoMediumItalic from './assets/fonts/Roboto-MediumItalic.ttf';
 import RobotoRegular from './assets/fonts/Roboto-Regular.ttf';
 import RobotoThin from './assets/fonts/Roboto-Thin.ttf';
 import RobotoThinItalic from './assets/fonts/Roboto-ThinItalic.ttf';
+
+import ScriptoRerumLogo from './assets/images/scriptorerum-logo.png';
 import AppNavigation from './navigation';
 
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    }
+    return Asset.fromModule(image).downloadAsync();
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
+
+async function loadAssetsAsync() {
+  const images = [ScriptoRerumLogo];
+
+  const fonts = [
+    {
+      ...Ionicons.font,
+      SpaceMono,
+      RobotoBlack,
+      RobotoBlackItalic,
+      RobotoBold,
+      RobotoBoldItalic,
+      RobotoItalic,
+      RobotoLight,
+      RobotoLightItalic,
+      RobotoMedium,
+      RobotoMediumItalic,
+      RobotoRegular,
+      RobotoThin,
+      RobotoThinItalic
+    }
+  ];
+
+  const imageAssets = cacheImages(images);
+  const fontAssets = cacheFonts(fonts);
+
+  await Promise.all([...imageAssets, ...fontAssets]);
+}
+
 export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
-  const [initialNavigationState, setInitialNavigationState] = React.useState();
-  const containerRef = React.useRef();
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+  const containerRef = useRef();
+  const [initialNavigationState, setInitialNavigationState] = useState();
   const { getInitialState } = useLinking(containerRef);
 
-  // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHide();
-
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
-
-        // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font,
-          'space-mono': SpaceMono,
-          'Roboto-Black': RobotoBlack,
-          'Roboto-Bold': RobotoBold,
-          'Roboto-BoldItalic': RobotoBoldItalic,
-          'Roboto-Italic': RobotoItalic,
-          'Roboto-Light': RobotoLight,
-          'Roboto-LightItalic': RobotoLightItalic,
-          'Roboto-Medium': RobotoMedium,
-          'Roboto-MediumItalic': RobotoMediumItalic,
-          'Roboto-Regular': RobotoRegular,
-          'Roboto-Thin': RobotoThin,
-          'Roboto-ThinItalic': RobotoThinItalic
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        // console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hide();
-      }
+  useEffect(() => {
+    async function setupInitialState() {
+      setInitialNavigationState(await getInitialState());
     }
 
-    loadResourcesAndDataAsync();
+    setupInitialState();
   }, []);
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null;
+    return <AppLoading startAsync={loadAssetsAsync} onFinish={() => setLoadingComplete(true)} />;
   }
+
   return (
     <View style={styles.container}>
-      {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
       <NavigationContainer
         ref={containerRef}
         initialState={initialNavigationState}
