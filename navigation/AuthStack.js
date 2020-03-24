9afef8523203eabc7af7a449c50ab5facfe94d7b
Nick Rameau <r@meau.me>
2020-03-24 14:08:35 -0400

    Reference the right component

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -5,7 +5,7 @@ import SignupScreen from '../screens/SignUpScreen';
 import LoginScreen from '../screens/LoginScreen';
 import ResetPasswordScreen from '../screens/ResetPasswordScreen';
 import ResetPasswordTwoScreen from '../screens/ResetPasswordTwoScreen';
-import MainStack from './MainStack';
+import BottomTab from './BottomTab';
 
 const Stack = createStackNavigator();
 
@@ -16,7 +16,9 @@ export default function AuthStack() {
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
       <Stack.Screen name="ResetPasswordTwoScreen" component={ResetPasswordTwoScreen} />
-      <Stack.Screen name="Main" component={MainStack} />
+      {/* We're adding the Home stack here because we don't have a store and logic yet. Once we do,
+      we will simply set the status of the logged in user via the state and the app will get them to the right screen */}
+      <Stack.Screen name="Home" component={BottomTab} />
     </Stack.Navigator>
   );
 }
