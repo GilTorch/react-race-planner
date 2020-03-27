b9ae728726948f87c6ec08067f01fc60d97a4ddd
Nick Rameau <r@meau.me>
2020-03-27 09:03:30 -0400

    DEMO: Display other screens in the navigation bar

--- navigation/BottomTab.js
+++ navigation/BottomTab.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../screens/HomeScreen';
+import UserPartOfStory from '../screens/story/UserPartOfStory';
+import StartedStory from '../screens/story/StartedStory';
+import InProgressStory from '../screens/story/InProgressStory';
+import CompletedStory from '../screens/story/CompletedStory';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +17,38 @@ export default function BottomTab() {
         component={HomeScreen}
         options={{
           tabBarLabel: 'Home'
+        }}
+      />
+
+      <Tab.Screen
+        name="UserPartOfStory"
+        component={UserPartOfStory}
+        options={{
+          tabBarLabel: 'PartOf'
+        }}
+      />
+
+      <Tab.Screen
+        name="InProgressStory"
+        component={InProgressStory}
+        options={{
+          tabBarLabel: 'InP'
+        }}
+      />
+
+      <Tab.Screen
+        name="StartedStory"
+        component={StartedStory}
+        options={{
+          tabBarLabel: 'Start'
+        }}
+      />
+
+      <Tab.Screen
+        name="CompletedStory"
+        component={CompletedStory}
+        options={{
+          tabBarLabel: 'Comp'
         }}
       />
     </Tab.Navigator>
