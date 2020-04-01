9f6fc54ad6aed134a9e53d88783621d9d4ef0b35
Nick Rameau <r@meau.me>
2020-04-01 10:16:52 -0400

    Style the bottom bar

--- navigation/BottomTab.js
+++ navigation/BottomTab.js
@@ -1,20 +1,51 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import { FontAwesome5 } from '@expo/vector-icons';
 import MainStack from './MainStack';
 import WritingStack from './WritingStack';
 import SettingsStack from './SettingsStack';
+import { SCREEN_HEIGHT } from '../utils/dimensions';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTab() {
   return (
-    <Tab.Navigator initialRouteName="Home">
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        labelPosition: 'beside-icon'
+      }}>
       <Tab.Screen
         name="Home"
         component={MainStack}
         options={{
-          tabBarLabel: 'Home'
+          tabBarLabel: ({ focused }) => {
+            if (!focused) return null;
+
+            return (
+              <Text
+                style={{
+                  paddingLeft: 15,
+                  textTransform: 'uppercase',
+                  fontWeight: 'bold',
+                  color: '#03A2A2'
+                }}>
+                Home
+              </Text>
+            );
+          },
+          tabBarIcon: ({ focused }) => {
+            return (
+              <FontAwesome5
+                style={{ fontSize: 20 }}
+                color={focused ? '#03A2A2' : '#707070'}
+                name="home"
+              />
+            );
+          }
         }}
       />
 
@@ -22,7 +53,30 @@ export default function BottomTab() {
         name="Writing"
         component={WritingStack}
         options={{
-          tabBarLabel: 'Writing'
+          tabBarLabel: ({ focused }) => {
+            if (!focused) return null;
+
+            return (
+              <Text
+                style={{
+                  paddingLeft: 15,
+                  textTransform: 'uppercase',
+                  fontWeight: 'bold',
+                  color: '#03A2A2'
+                }}>
+                Writing
+              </Text>
+            );
+          },
+          tabBarIcon: ({ focused }) => {
+            return (
+              <FontAwesome5
+                style={{ fontSize: 20 }}
+                color={focused ? '#03A2A2' : '#707070'}
+                name="pen-fancy"
+              />
+            );
+          }
         }}
       />
 
@@ -30,7 +84,30 @@ export default function BottomTab() {
         name="Settings"
         component={SettingsStack}
         options={{
-          tabBarLabel: 'Settings'
+          tabBarLabel: ({ focused }) => {
+            if (!focused) return null;
+
+            return (
+              <Text
+                style={{
+                  paddingLeft: 15,
+                  textTransform: 'uppercase',
+                  fontWeight: 'bold',
+                  color: '#03A2A2'
+                }}>
+                Settings
+              </Text>
+            );
+          },
+          tabBarIcon: ({ focused }) => {
+            return (
+              <FontAwesome5
+                style={{ fontSize: 20 }}
+                color={focused ? '#03A2A2' : '#707070'}
+                name="cog"
+              />
+            );
+          }
         }}
       />
     </Tab.Navigator>
