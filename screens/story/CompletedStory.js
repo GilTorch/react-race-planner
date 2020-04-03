88d2226fd09584c70bdcff1550986299983e0986
mcdowp <macdowell@noukod.com>
2020-04-02 21:02:07 -0400

    Minor fixes

--- screens/story/CompletedStory.js
+++ screens/story/CompletedStory.js
@@ -14,9 +14,9 @@ import { SCREEN_WIDTH } from '../../utils/dimensions';
 import { intros, endings } from '../../utils/data';
 
 const CompletedStory = ({ navigation }) => {
-  const [listView, setListView] = React.useState(false);
-  const icon = listView ? 'eye-slash' : 'eye';
-  const color = listView ? '#FFF' : '#EEE';
+  const [listMode, setListMode] = React.useState(false);
+  const icon = listMode ? 'eye-slash' : 'eye';
+  const color = listMode ? '#FFF' : '#EEE';
 
   navigation.setOptions({
     headerShown: false
@@ -90,7 +90,7 @@ const CompletedStory = ({ navigation }) => {
             </Surface>
 
             <Surface style={styles.surface}>
-              <TouchableRipple onPress={() => setListView(!listView)} style={{ flex: 1 }}>
+              <TouchableRipple onPress={() => setListMode(!listMode)} style={{ flex: 1 }}>
                 <View style={{ flexDirection: 'row', paddingHorizontal: 5 }}>
                   <FontAwesome
                     name="bars"
@@ -115,27 +115,27 @@ const CompletedStory = ({ navigation }) => {
       </Surface>
 
       <ScrollView>
-        <ProposedSection type="Intro" proposedBlocks={intros} listView={listView} />
+        <ProposedSection type="Intro" proposedBlocks={intros[1]} listMode={listMode} />
 
         <SmallAdvertisement />
-        <Round title="Round 1/11" subTitle="By stephanyE289" comments={0} listView={listView} />
-        <Round title="Round 2/11" subTitle="By Anonymous 8" comments={3} listView={listView} />
-        <Round title="Round 3/11" subTitle="By Marie Clark" comments={3} listView={listView} />
-        <Round title="Round 4/11" subTitle="By Anonymous 2" comments={10} listView={listView} />
+        <Round title="Round 1/11" subTitle="By stephanyE289" comments={0} listMode={listMode} />
+        <Round title="Round 2/11" subTitle="By Anonymous 8" comments={3} listMode={listMode} />
+        <Round title="Round 3/11" subTitle="By Marie Clark" comments={3} listMode={listMode} />
+        <Round title="Round 4/11" subTitle="By Anonymous 2" comments={10} listMode={listMode} />
         <HugeAdvertisement />
-        <Round title="Round 5/11" subTitle="By Anonymous 6" comments={1} listView={listView} />
-        <Round title="Round 6/11" subTitle="By Anonymous 5" comments={0} listView={listView} />
+        <Round title="Round 5/11" subTitle="By Anonymous 6" comments={1} listMode={listMode} />
+        <Round title="Round 6/11" subTitle="By Anonymous 5" comments={0} listMode={listMode} />
         <SmallAdvertisement />
-        <Round title="Round 7/11" subTitle="By Jesica Eloi" comments={0} listView={listView} />
-        <Round title="Round 8/11" subTitle="By Anonymous 11" comments={0} listView={listView} />
-        <Round title="Round 9/11" subTitle="By Anonymous 3" comments={33} listView={listView} />
-        <Round title="Round 10/11" subTitle="By Anonymous 4" comments={2} listView={listView} />
+        <Round title="Round 7/11" subTitle="By Jesica Eloi" comments={0} listMode={listMode} />
+        <Round title="Round 8/11" subTitle="By Anonymous 11" comments={0} listMode={listMode} />
+        <Round title="Round 9/11" subTitle="By Anonymous 3" comments={33} listMode={listMode} />
+        <Round title="Round 10/11" subTitle="By Anonymous 4" comments={2} listMode={listMode} />
         <HugeAdvertisement />
-        <Round title="Round 11/11" subTitle="By Anonymous 9" comments={0} listView={listView} />
+        <Round title="Round 11/11" subTitle="By Anonymous 9" comments={0} listMode={listMode} />
 
-        <ProposedSection type="Ending" proposedBlocks={endings} listView={listView} />
+        <ProposedSection type="Ending" proposedBlocks={endings} listMode={listMode} />
       </ScrollView>
-      {!listView && (
+      {!listMode && (
         <View
           style={{
             position: 'absolute',
