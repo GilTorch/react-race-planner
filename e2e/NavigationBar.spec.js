f60db3cd626b1c8effc522a4c1585e7c2af4877a
Nick Rameau <r@meau.me>
2020-04-09 15:05:56 -0400

    Hotfix

--- e2e/NavigationBar.spec.js
+++ e2e/NavigationBar.spec.js
@@ -8,25 +8,25 @@ describe('Navigation bar', () => {
     password: '**********'
   };
 
-  it('_should tap on the login link', async () => {
+  it('Should tap on the login link', async () => {
     await navigationBar.tapToGoOnLogingPage();
   });
 
-  it('_should ensure proper login credentials exitence', async () => {
+  it('Should ensure proper login credentials exitence', async () => {
     await navigationBar.enterLoginCredential(config);
   });
 
-  it('_should check that `home` icon exits and is visible in bottom tab', async () => {
+  it('Should check that `home` icon exits and is visible in bottom tab', async () => {
     await expect(navigationBar.home).toExist();
     await expect(navigationBar.home).toBeVisible();
   });
 
-  it('_should check that `writing` icon to not be visible in bottom tab', async () => {
+  it('Should check that `writing` icon to not be visible in bottom tab', async () => {
     // await expect(navigationBar.writing).toExist();
     await expect(navigationBar.writing).toBeNotVisible();
   });
 
-  it('_should check that `settings` icon to not be visible in bottom tab', async () => {
+  it('Should check that `settings` icon to not be visible in bottom tab', async () => {
     // await expect(navigationBar.settings).toExist();
     await expect(navigationBar.settings).toBeNotVisible();
   });
