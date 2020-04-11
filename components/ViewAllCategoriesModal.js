ba4d11b10360fc103b4f78bfca9ed8824b7e092c
Nick Rameau <r@meau.me>
2020-04-11 00:35:59 -0400

    Make modal taller and replace cross by cancel button

--- components/ViewAllCategoriesModal.js
+++ components/ViewAllCategoriesModal.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { FlatList, TouchableOpacity, View } from 'react-native';
 import { Modal, Portal } from 'react-native-paper';
 import PropTypes from 'prop-types';
-import { FontAwesome } from '@expo/vector-icons';
 
 import { SCREEN_HEIGHT } from '../utils/dimensions';
 import { genres } from '../utils/data';
@@ -15,44 +14,17 @@ const ViewAllCategoriesModal = ({ visible, dismiss }) => {
       <Modal style={{ overflow: 'hidden' }} visible={visible}>
         <View
           style={{
-            height: SCREEN_HEIGHT * .7,
+            height: SCREEN_HEIGHT * .85,
             backgroundColor: 'white',
             marginHorizontal: 20,
-            borderRadius: 6,
-            paddingBottom: 4
+            borderRadius: 6
           }}>
-          <View
-            style={{
-              marginTop: 10,
-              paddingRight: 10,
-              flexDirection: 'row',
-              justifyContent: 'flex-end',
-              shadowColor: '#000',
-              shadowOffset: {
-                width: 0,
-                height: 2
-              },
-              shadowOpacity: 0.25,
-              shadowRadius: 3.84
-            }}>
-            <TouchableOpacity
-              onPress={dismiss}
-              style={{
-                backgroundColor: '#03A2A2',
-                width: 30,
-                height: 30,
-                borderRadius: 4,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <FontAwesome color="#fff" size={25} name="close" />
-            </TouchableOpacity>
-          </View>
           <View
             style={{
               flexDirection: 'column',
               justifyContent: 'center',
               alignItems: 'center',
+              marginTop: 10,
               paddingBottom: 10
             }}>
             <Text style={{ color: '#5A7582' }}>Start a New Story</Text>
@@ -66,6 +38,13 @@ const ViewAllCategoriesModal = ({ visible, dismiss }) => {
             keyExtractor={item => item.id.toString()}
             renderItem={({ item }) => <CategoryGenre genre={item} />}
           />
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              onPress={dismiss}
+              style={{ ...styles.button, backgroundColor: '#F44336' }}>
+              <Text style={styles.buttonText}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </Modal>
     </Portal>
@@ -76,5 +55,33 @@ ViewAllCategoriesModal.propTypes = {
   visible: PropTypes.bool.isRequired,
   dismiss: PropTypes.func.isRequired
 };
+
+const styles = {
+  buttonContainer: {
+    alignItems: 'flex-end',
+    paddingHorizontal: 20,
+    marginVertical: 7
+  },
+  button: {
+    width: 120,
+    backgroundColor: '#03A2A2',
+    height: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 20,
+    borderRadius: 6,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18
+  }
+}
 
 export default ViewAllCategoriesModal;
