884b311141f5293f6a7527345088f6e03586619f
dowpm <mcdowpm@gmail.com>
2020-04-16 18:08:37 -0400

    Handle fields validations and jump between textInput fields

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { View, ScrollView, Image, TextInput, StyleSheet, StatusBar } from 'react-native';
+import {
+  View,
+  ScrollView,
+  Image,
+  TextInput,
+  StyleSheet,
+  StatusBar,
+  Platform,
+  KeyboardAvoidingView
+} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Entypo } from '@expo/vector-icons';
 import PropTypes from 'prop-types';
@@ -8,6 +17,7 @@ import { useFocusEffect } from '@react-navigation/native';
 import SRLogo from '../assets/images/scriptorerum-logo.png';
 import Text from '../components/CustomText';
 import GoogleColorfulIcon from '../components/GoogleColorfulIcon';
+import { signupSchema } from '../utils/validators';
 
 const SignupScreen = ({ navigation }) => {
   useFocusEffect(
@@ -16,122 +26,225 @@ const SignupScreen = ({ navigation }) => {
     }, [])
   );
 
+  const inputs = {};
+  let scrollRef = React.useRef();
+  const [form, setState] = React.useState({});
+  const [errors, setErrors] = React.useState({});
+  const focusNextField = name => inputs[name].focus();
+
+  const handleSubmit = () => {
+    scrollRef.scrollTo({ y: 230, animated: true });
+    setErrors({});
+
+    signupSchema
+      .validate(form, { abortEarly: false })
+      .then(value => console.log(value))
+      .catch(err => {
+        err.errors.map(e => {
+          const [inputName, msg] = e.split(':');
+          setErrors(prev => ({ ...prev, [inputName]: msg }));
+          return e;
+        });
+      });
+  };
+
   return (
-    <ScrollView contentContainerStyle={{ backgroundColor: 'white' }}>
-      <View style={styles.container}>
-        <Image source={SRLogo} resizeMode="contain" style={styles.logo} />
-        <View style={styles.headlineContainer}>
-          <Text type="medium" style={styles.headline}>
-            Create an Account
-          </Text>
-        </View>
-        <View style={styles.form}>
-          <View style={styles.formGroup}>
-            <View style={styles.labelContainer}>
-              <Text type="medium" style={styles.label}>
-                Username
-              </Text>
-            </View>
-            <View style={styles.inputContainer}>
-              <TextInput style={styles.input} />
-            </View>
-          </View>
-          <View style={styles.formGroup}>
-            <View style={styles.labelContainer}>
-              <Text type="medium" style={styles.label}>
-                First Name
-              </Text>
-            </View>
-            <View style={styles.inputContainer}>
-              <TextInput style={styles.input} />
-            </View>
-          </View>
-          <View style={styles.formGroup}>
-            <View style={styles.labelContainer}>
-              <Text type="medium" style={styles.label}>
-                Last Name
-              </Text>
-            </View>
-            <View style={styles.inputContainer}>
-              <TextInput style={styles.input} />
-            </View>
-          </View>
-          <View style={styles.formGroup}>
-            <View style={styles.labelContainer}>
-              <Text type="medium" style={styles.label}>
-                Email
-              </Text>
-            </View>
-            <View style={styles.inputContainer}>
-              <TextInput style={styles.input} />
-            </View>
-          </View>
-          <View style={styles.formGroup}>
-            <View style={styles.labelContainer}>
-              <Text type="medium" style={styles.label}>
-                Password
-              </Text>
-            </View>
-            <View style={styles.inputContainer}>
-              <TextInput style={styles.input} />
-            </View>
-          </View>
-          <View style={styles.formGroup}>
-            <View style={styles.labelContainer}>
-              <Text type="medium" style={styles.label}>
-                Confirm Password
-              </Text>
-            </View>
-            <View style={styles.inputContainer}>
-              <TextInput style={styles.input} />
-            </View>
-          </View>
-          <TouchableOpacity style={styles.submitButton}>
-            <Text type="medium" style={styles.submitButtonText}>
-              Sign Up
-            </Text>
-          </TouchableOpacity>
-          <View style={styles.loginWithSocialMediaTextContainer}>
-            <Text type="medium" style={{ color: '#7F8FA4', fontWeight: 'bold' }}>
-              Or login via social networks
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' && 'padding'}>
+      <ScrollView
+        ref={scroll => {
+          scrollRef = scroll;
+        }}
+        contentContainerStyle={{ backgroundColor: 'white' }}>
+        <View style={styles.container}>
+          <Image source={SRLogo} resizeMode="contain" style={styles.logo} />
+          <View style={styles.headlineContainer}>
+            <Text type="medium" style={styles.headline}>
+              Create an Account
             </Text>
           </View>
-          <View style={styles.socialMediaButtonsContainer}>
-            <TouchableOpacity
-              style={{
-                backgroundColor: '#3ABDFF',
-                ...styles.socialMediaButton
-              }}>
-              <Entypo name="twitter-with-circle" size={24} color="#fff" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{
-                backgroundColor: '#1382D5',
-                ...styles.socialMediaButton
-              }}>
-              <Entypo name="facebook-with-circle" size={24} color="#fff" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{
-                backgroundColor: '#e6e6e6',
-                ...styles.socialMediaButton
-              }}>
-              <GoogleColorfulIcon />
-            </TouchableOpacity>
-          </View>
-          <View style={{ marginTop: 20, marginBottom: 40, flexDirection: 'row' }}>
-            <Text style={{ color: '#7F8FA4' }}>Already a member? </Text>
-            <TouchableOpacity
-              onPress={() => navigation.navigate('Login')}
-              style={styles.goToLoginPageButton}>
-              <View>
-                <Text style={styles.goToLoginPageButtonText}>Log in</Text>
+          <View style={styles.form}>
+            <View style={styles.formGroup}>
+              <View style={styles.labelContainer}>
+                <Text type="medium" style={styles.label}>
+                  Username
+                </Text>
               </View>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  onChangeText={text => setState({ ...form, username: text })}
+                  value={form.username}
+                  onSubmitEditing={() => focusNextField('firstName')}
+                  blurOnSubmit={false}
+                  returnKeyType="next"
+                  style={[styles.input, errors.username && styles.errorInput]}
+                />
+              </View>
+              {errors.username && (
+                <Text style={{ fontSize: 11, color: 'red' }}>{errors.username}</Text>
+              )}
+            </View>
+            <View style={styles.formGroup}>
+              <View style={styles.labelContainer}>
+                <Text type="medium" style={styles.label}>
+                  First Name
+                </Text>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  onChangeText={text => setState({ ...form, firstName: text })}
+                  value={form.firstName}
+                  onSubmitEditing={() => focusNextField('lastName')}
+                  blurOnSubmit={false}
+                  ref={input => {
+                    inputs.firstName = input;
+                  }}
+                  returnKeyType="next"
+                  style={[styles.input, errors.firstName && styles.errorInput]}
+                />
+              </View>
+              {errors.firstName && (
+                <Text style={{ fontSize: 11, color: 'red' }}>{errors.firstName}</Text>
+              )}
+            </View>
+            <View style={styles.formGroup}>
+              <View style={styles.labelContainer}>
+                <Text type="medium" style={styles.label}>
+                  Last Name
+                </Text>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  onChangeText={text => setState({ ...form, lastName: text })}
+                  value={form.lastName}
+                  onSubmitEditing={() => focusNextField('email')}
+                  blurOnSubmit={false}
+                  ref={input => {
+                    inputs.lastName = input;
+                  }}
+                  returnKeyType="next"
+                  style={[styles.input, errors.lastName && styles.errorInput]}
+                />
+              </View>
+              {errors.lastName && (
+                <Text style={{ fontSize: 11, color: 'red' }}>{errors.lastName}</Text>
+              )}
+            </View>
+            <View style={styles.formGroup}>
+              <View style={styles.labelContainer}>
+                <Text type="medium" style={styles.label}>
+                  Email
+                </Text>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  onChangeText={text => setState({ ...form, email: text })}
+                  value={form.email}
+                  onSubmitEditing={() => focusNextField('password')}
+                  blurOnSubmit={false}
+                  ref={input => {
+                    inputs.email = input;
+                  }}
+                  keyboardType="email-address"
+                  returnKeyType="next"
+                  style={[styles.input, errors.email && styles.errorInput]}
+                />
+              </View>
+              {errors.email && <Text style={{ fontSize: 11, color: 'red' }}>{errors.email}</Text>}
+            </View>
+            <View style={styles.formGroup}>
+              <View style={styles.labelContainer}>
+                <Text type="medium" style={styles.label}>
+                  Password
+                </Text>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  onChangeText={text => setState({ ...form, password: text })}
+                  value={form.password}
+                  onSubmitEditing={() => focusNextField('password2')}
+                  blurOnSubmit={false}
+                  ref={input => {
+                    inputs.password = input;
+                  }}
+                  secureTextEntry
+                  returnKeyType="next"
+                  style={[styles.input, errors.password && styles.errorInput]}
+                />
+              </View>
+              {errors.password && (
+                <Text style={{ fontSize: 11, color: 'red' }}>{errors.password}</Text>
+              )}
+            </View>
+            <View style={styles.formGroup}>
+              <View style={styles.labelContainer}>
+                <Text type="medium" style={styles.label}>
+                  Confirm Password
+                </Text>
+              </View>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  onChangeText={text => setState({ ...form, password2: text })}
+                  value={form.password2}
+                  onSubmitEditing={handleSubmit}
+                  ref={input => {
+                    inputs.password2 = input;
+                  }}
+                  secureTextEntry
+                  returnKeyType="done"
+                  style={[styles.input, errors.password2 && styles.errorInput]}
+                />
+              </View>
+              {errors.password2 && (
+                <Text style={{ fontSize: 11, color: 'red' }}>{errors.password2}</Text>
+              )}
+            </View>
+            <TouchableOpacity onPress={handleSubmit} style={styles.submitButton}>
+              <Text type="medium" style={styles.submitButtonText}>
+                Sign Up
+              </Text>
             </TouchableOpacity>
+            <View style={styles.loginWithSocialMediaTextContainer}>
+              <Text type="medium" style={{ color: '#7F8FA4', fontWeight: 'bold' }}>
+                Or login via social networks
+              </Text>
+            </View>
+            <View style={styles.socialMediaButtonsContainer}>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: '#3ABDFF',
+                  ...styles.socialMediaButton
+                }}>
+                <Entypo name="twitter-with-circle" size={24} color="#fff" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: '#1382D5',
+                  ...styles.socialMediaButton
+                }}>
+                <Entypo name="facebook-with-circle" size={24} color="#fff" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: '#e6e6e6',
+                  ...styles.socialMediaButton
+                }}>
+                <GoogleColorfulIcon />
+              </TouchableOpacity>
+            </View>
+            <View style={{ marginTop: 20, marginBottom: 40, flexDirection: 'row' }}>
+              <Text style={{ color: '#7F8FA4' }}>Already a member? </Text>
+              <TouchableOpacity
+                onPress={() => navigation.navigate('Login')}
+                style={styles.goToLoginPageButton}>
+                <View>
+                  <Text style={styles.goToLoginPageButtonText}>Log in</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -181,6 +294,10 @@ const styles = StyleSheet.create({
     paddingLeft: 8,
     flex: 1,
     height: 35.43
+  },
+  errorInput: {
+    borderColor: 'red',
+    borderBottomWidth: 1
   },
   form: {
     width: '75%'
