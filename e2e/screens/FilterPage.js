fdb5a3a90a9894f508bad5723800e8ac3af6bdcc
CornetS28 <corsam28@gmail.com>
2020-04-21 11:27:06 -0400

    fix eslint errors

--- e2e/screens/FilterPage.js
+++ e2e/screens/FilterPage.js
@@ -2,54 +2,54 @@
 /* eslint-disable class-methods-use-this */
 class FilterPage {
   async enterLoginCredential(config) {
-    await element(by.id("login-user-name")).typeText(config.username);
-    await element(by.id("login-user-name")).tapReturnKey();
-    await element(by.id("login-password")).typeText(config.password);
-    await element(by.id("login-password")).tapReturnKey();
-    await element(by.id("login-button")).tap();
-    waitFor(element(by.id("home"))).toBeVisible();
-    waitFor(element(by.id("home-icon"))).toBeVisible();
-    waitFor(element(by.id("filter-button"))).toBeVisible();
-    await element(by.id("filter-button")).toBeVisible();
+    await element(by.id('login-user-name')).typeText(config.username);
+    await element(by.id('login-user-name')).tapReturnKey();
+    await element(by.id('login-password')).typeText(config.password);
+    await element(by.id('login-password')).tapReturnKey();
+    await element(by.id('login-button')).tap();
+    waitFor(element(by.id('home'))).toBeVisible();
+    waitFor(element(by.id('home-icon'))).toBeVisible();
+    waitFor(element(by.id('filter-button'))).toBeVisible();
+    await element(by.id('filter-button')).toBeVisible();
   }
 
   async filterButton() {
-    waitFor(element(by.id("filter-button"))).toBeVisible();
-    await element(by.id("filter-button")).tap();
+    waitFor(element(by.id('filter-button'))).toBeVisible();
+    await element(by.id('filter-button')).tap();
   }
 
   get status() {
-    return element(by.id("status"));
+    return element(by.id('status'));
   }
 
   async resetLink() {
-    waitFor(element(by.id("reset"))).toBeVisible();
-    await element(by.id("reset")).tap();
+    waitFor(element(by.id('reset'))).toBeVisible();
+    await element(by.id('reset')).tap();
   }
 
   async doneLink() {
-    waitFor(element(by.id("done"))).toBeVisible();
-    await element(by.id("done")).tap();
+    waitFor(element(by.id('done'))).toBeVisible();
+    await element(by.id('done')).tap();
   }
 
   get selectAllOne() {
-    return element(by.id("select-all-part-one"));
+    return element(by.id('select-all-part-one'));
   }
 
   get selectAllTwo() {
-    return element(by.id("select-all-part-two"));
+    return element(by.id('select-all-part-two'));
   }
 
   get authorsSlider() {
-    return element(by.id("authors-slider"));
+    return element(by.id('authors-slider'));
   }
 
   get selectStatus() {
-    return element(by.id("selected-status"));
+    return element(by.id('selected-status'));
   }
 
   get selectGenres() {
-    return element(by.id("selected-genres"));
+    return element(by.id('selected-genres'));
   }
 }
 
