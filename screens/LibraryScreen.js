8336ce430f30305e5e9c08abf0ca69c083070abd
Nick Rameau <r@meau.me>
2020-04-22 22:57:16 -0400

    Library and Writing screens -> Connect Search component to the Search button

--- screens/LibraryScreen.js
+++ screens/LibraryScreen.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ScrollView, View, StyleSheet, StatusBar } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { AntDesign, FontAwesome, SimpleLineIcons } from '@expo/vector-icons';
-import { Surface } from 'react-native-paper';
+import { Surface, Searchbar } from 'react-native-paper';
 import PropTypes from 'prop-types';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Constants from 'expo-constants';
@@ -13,6 +13,8 @@ import { stories, genres } from '../utils/data';
 import Story from '../components/stories/Story';
 
 const LibraryScreen = ({ navigation }) => {
+  const [searchBarVisible, setSearchBarVisible] = useState(false);
+
   navigation.setOptions({
     headerShown: false
   });
@@ -89,43 +91,91 @@ const LibraryScreen = ({ navigation }) => {
           </ScrollView>
         </Surface>
 
-        <View
-          style={{
-            marginHorizontal: 20,
-            marginTop: 20,
-            marginBottom: 25,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-          }}>
-          <Text type="medium" style={{ ...styles.headline, fontSize: 18 }}>
-            All Stories
-          </Text>
+        {searchBarVisible && (
           <View
             style={{
               flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignSelf: 'stretch'
+              justifyContent: 'space-around',
+              alignItems: 'center',
+              marginTop: 20,
+              marginLeft: 20,
+              marginRight: 20,
+              marginBottom: 15
             }}>
-            <Surface style={{ borderRadius: 5, elevation: 2, padding: 4, marginRight: 10 }}>
+            <View style={{ flex: 8 }}>
+              <Searchbar style={{ height: 40, paddingTop: 3, elevation: 2 }} iconColor="#03A2A2" />
+            </View>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}>
+              <TouchableOpacity onPress={() => setSearchBarVisible(false)}>
+                <AntDesign size={20} name="closecircleo" color="#03A2A2" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+
+        {!searchBarVisible && (
+          <View
+            style={{
+              marginLeft: 20,
+              marginRight: 15,
+              marginTop: 20,
+              marginBottom: 15,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between'
+            }}>
+            <Text type="medium" style={{ ...styles.headline, fontSize: 18 }}>
+              All Stories
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row'
+              }}>
               <TouchableOpacity
-                style={{ flexDirection: 'row', alignItems: 'center' }}
+                style={{ borderRadius: 5, padding: 5, flex: 1 }}
                 onPress={() => {
                   navigation.push('FilterScreen');
                 }}>
-                <AntDesign color="#5A7582" size={18} name="filter" />
-                <Text type="bold" style={{ fontSize: 12, color: '#5A7582' }}>
-                  FILTER
-                </Text>
+                <Surface
+                  style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    borderRadius: 5,
+                    elevation: 2,
+                    padding: 5
+                  }}>
+                  <AntDesign color="#5A7582" size={18} name="filter" />
+                  <Text type="bold" style={{ fontSize: 12, color: '#5A7582' }}>
+                    FILTER
+                  </Text>
+                </Surface>
               </TouchableOpacity>
-            </Surface>
-            <Surface style={{ borderRadius: 5, elevation: 2, padding: 5 }}>
-              <TouchableOpacity>
-                <FontAwesome size={14} color="#5A7582" name="search" />
+
+              <TouchableOpacity
+                style={{ borderRadius: 5, flex: 1, padding: 5 }}
+                onPress={() => setSearchBarVisible(true)}>
+                <Surface
+                  style={{
+                    borderRadius: 5,
+                    elevation: 2,
+                    paddingHorizontal: 9,
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    padding: 5
+                  }}>
+                  <FontAwesome size={14} color="#5A7582" name="search" />
+                </Surface>
               </TouchableOpacity>
-            </Surface>
+            </View>
           </View>
-        </View>
+        )}
 
         {completedStories.map((story, index) => (
           <Story
