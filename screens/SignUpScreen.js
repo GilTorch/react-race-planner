385f96d7ea809126dad3fdf4e24460f8ea1c1e06
dowpm <mcdowpm@gmail.com>
2020-04-30 01:22:40 -0400

    Implement signup with google

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { View, ScrollView, Image, TextInput, StyleSheet, StatusBar } from 'react-native';
+import {
+  View,
+  ScrollView,
+  Image,
+  TextInput,
+  StyleSheet,
+  StatusBar,
+  ActivityIndicator,
+  Modal,
+  Platform
+} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Entypo } from '@expo/vector-icons';
 import PropTypes from 'prop-types';
 import { useFocusEffect } from '@react-navigation/native';
+import * as Google from 'expo-google-app-auth';
 
 import SRLogo from '../assets/images/scriptorerum-logo.png';
 import Text from '../components/CustomText';
 import GoogleColorfulIcon from '../components/GoogleColorfulIcon';
+import { signupSchema } from '../utils/validators';
 
 const SignupScreen = ({ navigation }) => {
   useFocusEffect(
@@ -16,8 +28,73 @@ const SignupScreen = ({ navigation }) => {
     }, [])
   );
 
+  const [form, setState] = React.useState({});
+  const [errors, setErrors] = React.useState({});
+  const [loading, setLoading] = React.useState(false);
+  const [socialMediaName, setSocialMediaName] = React.useState('');
+
+  React.useEffect(() => {
+    if (form.socialAccount) {
+      signup();
+    }
+  }, [form.socialAccount]);
+
+  const formatFormErrors = e => {
+    const [inputName, msg] = e.split(':');
+    setErrors(prev => ({ ...prev, [inputName]: msg }));
+  };
+
+  const signup = () => {
+    setErrors({});
+
+    signupSchema
+      .validate(form, { abortEarly: false })
+      .then(value => {
+        setLoading(false);
+        // dispatch(signUpUser(value));
+      })
+      .catch(err => {
+        err.errors.map(formatFormErrors);
+        setLoading(false);
+      });
+  };
+
+  async function signInWithGoogleAsync() {
+    setLoading(true);
+
+    try {
+      const result = await Google.logInAsync({
+        androidClientId: '706451172353-809kg5r2ha357574ii7iupnbf8nv8qvo.apps.googleusercontent.com',
+        scopes: ['profile', 'email']
+      });
+
+      if (result.type === 'success') {
+        setState({
+          username: '',
+          firstName: result.user.givenName,
+          lastName: result.user.familyName,
+          email: result.user.email,
+          socialAccount: true,
+          googleAcountId: result.user.id
+        });
+        setSocialMediaName('Google');
+      }
+
+      setLoading(false);
+      return null;
+    } catch (e) {
+      setLoading(false);
+      return { error: true };
+    }
+  }
+
   return (
     <ScrollView contentContainerStyle={{ backgroundColor: 'white' }}>
+      <Modal transparent visible={loading}>
+        <View style={styles.spiner}>
+          <ActivityIndicator size={Platform.isAndroid ? 60 : 'large'} color="#f8f8f8" />
+        </View>
+      </Modal>
       <View style={styles.container}>
         <Image testID="logo" source={SRLogo} resizeMode="contain" style={styles.logo} />
         <View testID="create-account-text" style={styles.headlineContainer}>
@@ -33,8 +110,15 @@ const SignupScreen = ({ navigation }) => {
               </Text>
             </View>
             <View style={styles.inputContainer}>
-              <TextInput testID="user-name" style={styles.input} />
+              <TextInput
+                testID="user-name"
+                value={form.username}
+                style={[styles.input, errors.username && styles.errorInput]}
+              />
             </View>
+            {errors.username && (
+              <Text style={{ fontSize: 11, color: 'red' }}>{errors.username}</Text>
+            )}
           </View>
           <View style={styles.formGroup}>
             <View style={styles.labelContainer}>
@@ -43,8 +127,15 @@ const SignupScreen = ({ navigation }) => {
               </Text>
             </View>
             <View style={styles.inputContainer}>
-              <TextInput testID="first-name" style={styles.input} />
+              <TextInput
+                testID="first-name"
+                value={form.firstName}
+                style={[styles.input, errors.firstName && styles.errorInput]}
+              />
             </View>
+            {errors.firstName && (
+              <Text style={{ fontSize: 11, color: 'red' }}>{errors.firstName}</Text>
+            )}
           </View>
           <View style={styles.formGroup}>
             <View style={styles.labelContainer}>
@@ -53,8 +144,15 @@ const SignupScreen = ({ navigation }) => {
               </Text>
             </View>
             <View style={styles.inputContainer}>
-              <TextInput testID="last-name" style={styles.input} />
+              <TextInput
+                testID="last-name"
+                value={form.lastName}
+                style={[styles.input, errors.lastName && styles.errorInput]}
+              />
             </View>
+            {errors.lastName && (
+              <Text style={{ fontSize: 11, color: 'red' }}>{errors.lastName}</Text>
+            )}
           </View>
           <View style={styles.formGroup}>
             <View style={styles.labelContainer}>
@@ -63,33 +161,49 @@ const SignupScreen = ({ navigation }) => {
               </Text>
             </View>
             <View style={styles.inputContainer}>
-              <TextInput testID="email-address" style={styles.input} />
+              <TextInput
+                testID="email-address"
+                value={form.email}
+                style={[styles.input, errors.email && styles.errorInput]}
+              />
             </View>
+            {errors.email && <Text style={{ fontSize: 11, color: 'red' }}>{errors.email}</Text>}
           </View>
-          <View style={styles.formGroup}>
-            <View style={styles.labelContainer}>
-              <Text type="medium" style={styles.label}>
-                Password
+          {!form.socialAccount && (
+            <>
+              <View style={styles.formGroup}>
+                <View style={styles.labelContainer}>
+                  <Text type="medium" style={styles.label}>
+                    Password
+                  </Text>
+                </View>
+                <View style={styles.inputContainer}>
+                  <TextInput testID="password" style={styles.input} />
+                </View>
+              </View>
+              <View style={styles.formGroup}>
+                <View style={styles.labelContainer}>
+                  <Text testID="password-confirmation" type="medium" style={styles.label}>
+                    Confirm Password
+                  </Text>
+                </View>
+                <View style={styles.inputContainer}>
+                  <TextInput testID="password-confirmation" style={styles.input} />
+                </View>
+              </View>
+            </>
+          )}
+          <TouchableOpacity style={styles.submitButton}>
+            {form.socialAccount && (
+              <Text type="medium" style={styles.submitButtonText}>
+                Continue signup with {socialMediaName}
               </Text>
-            </View>
-            <View style={styles.inputContainer}>
-              <TextInput testID="password" style={styles.input} />
-            </View>
-          </View>
-          <View style={styles.formGroup}>
-            <View style={styles.labelContainer}>
-              <Text testID="password-confirmation" type="medium" style={styles.label}>
-                Confirm Password
+            )}
+            {!form.socialAccount && (
+              <Text type="medium" style={styles.submitButtonText}>
+                Sign Up
               </Text>
-            </View>
-            <View style={styles.inputContainer}>
-              <TextInput testID="password-confirmation" style={styles.input} />
-            </View>
-          </View>
-          <TouchableOpacity testID="sign-up-button" style={styles.submitButton}>
-            <Text type="medium" style={styles.submitButtonText}>
-              Sign Up
-            </Text>
+            )}
           </TouchableOpacity>
           <View style={styles.loginWithSocialMediaTextContainer}>
             <Text type="medium" style={{ color: '#7F8FA4', fontWeight: 'bold' }}>
@@ -114,6 +228,7 @@ const SignupScreen = ({ navigation }) => {
               <Entypo name="facebook-with-circle" size={24} color="#fff" />
             </TouchableOpacity>
             <TouchableOpacity
+              onPress={signInWithGoogleAsync}
               testID="google-icon-btn"
               style={{
                 backgroundColor: '#e6e6e6',
@@ -143,6 +258,12 @@ SignupScreen.propTypes = {
 };
 
 const styles = StyleSheet.create({
+  spiner: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   container: {
     backgroundColor: 'white',
     width: '100%',
