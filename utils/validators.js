9736414787b4fc527c11a48df00e5c1f1c96c2a0
Nick Rameau <r@meau.me>
2020-05-07 21:13:26 -0400

    Remove too strict validations and hotfixes

--- utils/validators.js
+++ utils/validators.js
@@ -5,17 +5,10 @@ import * as yup from 'yup';
 export const signupSchema = yup.object().shape({
   username: yup
     .string()
-    .label('email')
-    .max(15, 'Must be 15 characters or less')
+    .min(3, 'Must be at least 3 characters')
     .required('Username is required.'),
-  firstName: yup
-    .string()
-    .max(20, 'Must be 20 characters or less')
-    .required('First Name is required'),
-  lastName: yup
-    .string()
-    .max(15, 'Must be 15 characters or less')
-    .required('Last Name is required'),
+  firstName: yup.string().required('First Name is required'),
+  lastName: yup.string().required('Last Name is required'),
   email: yup
     .string()
     .email('Invalid email address')
@@ -25,16 +18,16 @@ export const signupSchema = yup.object().shape({
     then: yup.string(),
     otherwise: yup
       .string()
-      .min(8, 'password:Password should be at last 8 characters long')
-      .required('password:Password is required')
+      .min(8, 'Password must be at last 8 characters long')
+      .required('Password is required')
   }),
   password2: yup.string().when('socialAccount', {
     is: true,
     then: yup.string(),
     otherwise: yup
       .string()
-      .oneOf([yup.ref('password'), null], "password2:Password don't match")
-      .required('password2:Confirm Password is required')
+      .oneOf([yup.ref('password'), null], "Password confirmation doesn't match the password")
+      .required('Password confirmation is required')
   }),
   socialAccount: yup.boolean()
 });
