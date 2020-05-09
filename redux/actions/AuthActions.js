6908621f6eaf1da44850148780ec44c6b16005d5
Nick Rameau <r@meau.me>
2020-05-08 21:03:00 -0400

    Minor refactoring

--- redux/actions/AuthActions.js
+++ redux/actions/AuthActions.js
@@ -1,35 +1,18 @@
 import axios from '../../services/axiosService';
 import { Auth } from './types';
-import { NEW_SESSION_URL } from '../../api/urls';
 
-export const signUpUser = ({
-  username,
-  firstName,
-  lastName,
-  email,
-  password,
-  googleAccountId
-}) => dispatch => {
+export const signUpUser = data => dispatch => {
   dispatch({ type: Auth.SIGN_UP_ATTEMPT });
 
-  return axios
-    .post(NEW_SESSION_URL, {
-      username,
-      firstName,
-      lastName,
-      email,
-      password,
-      googleAccountId
-    })
-    .catch(error => {
-      // TODO: handle server error, right now got an error status 500 for duplicated
-      // username/email
-      // console.log(error);
-      dispatch({
-        type: Auth.SIGN_UP_FAIL,
-        payload: error.response?.data || { message: 'Unexpected Error from the app' }
-      });
+  return axios.post('/users/signup', data).catch(error => {
+    // TODO: handle server error, right now got an error status 500 for duplicated
+    // username/email
+    // console.log(error);
+    dispatch({
+      type: Auth.SIGN_UP_FAIL,
+      payload: error.response?.data || { message: 'Unexpected Error from the app' }
     });
+  });
 };
 
 export const clearRequestError = () => dispatch => {
