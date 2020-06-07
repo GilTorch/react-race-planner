73f2637511a05e8a38dea9795808747a7a67c195
Nick Rameau <r@meau.me>
2020-06-07 14:00:58 -0400

    Get rid of unused action

--- redux/actions/AuthActions.js
+++ redux/actions/AuthActions.js
@@ -82,27 +82,3 @@ export const passwordResetVerificationAction = data => dispatch => {
 export const logoutAction = () => dispatch => {
   dispatch({ type: Auth.LOGOUT });
 };
-
-export const updateUserProfile = data => dispatch => {
-  dispatch({ type: Auth.START_A_REQUEST });
-
-  return axios
-    .put(`/users/${data.id}`, data, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    })
-    .then(response => {
-      dispatch({ type: Auth.UPDATE_PROFILE_SUCCESS, payload: response.data });
-      return response.data;
-    })
-    .catch(error => {
-      alert(JSON.stringify(error.response));
-
-      dispatch({
-        type: Auth.UPDATE_PROFILE_FAILURE,
-        payload: error.response?.data || { message: 'Unexpected Error from the app' }
-      });
-      return null;
-    });
-};
