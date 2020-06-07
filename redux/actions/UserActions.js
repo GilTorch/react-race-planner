f21fb934352591e465158fef5c54b9ad85fe2848
Nick Rameau <r@meau.me>
2020-06-07 19:40:26 -0400

    Rename action and hotfix

--- redux/actions/UserActions.js
+++ redux/actions/UserActions.js
@@ -2,20 +2,17 @@
 import axios from '../../services/axiosService';
 import { Auth } from './types';
 
-export const updateUserProfile = dataObj => dispatch => {
+export const userUpdateAction = dataObj => dispatch => {
   dispatch({ type: Auth.START_A_REQUEST });
 
   return axios
     .put(`/users/${dataObj.id}`, dataObj.data)
     .then(response => {
       dispatch({ type: Auth.UPDATE_PROFILE_SUCCESS, payload: response.data });
-      return response.data;
     })
     .catch(error => {
-      dispatch({
-        type: Auth.UPDATE_PROFILE_FAILURE,
-        payload: error.response?.data || { message: 'Unexpected Error from the app' }
-      });
-      return null;
+      dispatch({ type: Auth.UPDATE_PROFILE_FAILURE });
+
+      throw error.response?.data;
     });
 };
