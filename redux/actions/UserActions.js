60847415f0f0ed8655466c0c085c954149db49ab
Nick Rameau <r@meau.me>
2020-06-07 18:04:26 -0400

    Send the correct data

--- redux/actions/UserActions.js
+++ redux/actions/UserActions.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import axios from '../../services/axiosService';
 import { Auth } from './types';
 
@@ -5,19 +6,12 @@ export const updateUserProfile = data => dispatch => {
   dispatch({ type: Auth.START_A_REQUEST });
 
   return axios
-    .put(`/users/${data.id}`, data, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    })
+    .put(`/users/${data.id}`, data.formData)
     .then(response => {
-      alert('got them');
       dispatch({ type: Auth.UPDATE_PROFILE_SUCCESS, payload: response.data });
       return response.data;
     })
     .catch(error => {
-      alert(JSON.stringify(error));
-
       dispatch({
         type: Auth.UPDATE_PROFILE_FAILURE,
         payload: error.response?.data || { message: 'Unexpected Error from the app' }
