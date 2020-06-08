5dc728d87d9fee6796bdc01f5259d6fde0b976a5
Nick Rameau <r@meau.me>
2020-06-08 17:29:27 -0400

    Improve header

--- screens/WebViewScreen.js
+++ screens/WebViewScreen.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, StatusBar } from 'react-native';
+import { View, StatusBar, SafeAreaView } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import { Surface, IconButton } from 'react-native-paper';
 import { WebView } from 'react-native-webview';
 import { LinearGradient } from 'expo-linear-gradient';
-import Constants from 'expo-constants';
 
 import Text from '../components/CustomText';
 
@@ -28,22 +27,18 @@ const WebViewScreen = ({ navigation, route }) => {
         style={{
           elevation: 3
         }}>
-        <LinearGradient
-          colors={['#03a2a2', '#23c2c2']}
-          locations={[0.5, 1]}
-          style={{
-            paddingBottom: Constants.statusBarHeight,
-            paddingTop: Constants.statusBarHeight * 2
-          }}>
-          <IconButton
-            onPress={() => navigation.goBack()}
-            icon="arrow-left"
-            style={{ position: 'absolute', top: 85 }}
-            color="white"
-          />
-          <Text type="bold" style={{ color: 'white', fontSize: 18, alignSelf: 'center' }}>
-            {title}
-          </Text>
+        <LinearGradient colors={['#03a2a2', '#23c2c2']} locations={[0.5, 1]}>
+          <SafeAreaView
+            style={{
+              alignItems: 'center',
+              flexDirection: 'row',
+              justifyContent: 'flex-start'
+            }}>
+            <IconButton onPress={() => navigation.goBack()} icon="arrow-left" color="white" />
+            <Text type="bold" style={{ color: 'white', marginLeft: 10, fontSize: 18 }}>
+              {title}
+            </Text>
+          </SafeAreaView>
         </LinearGradient>
       </Surface>
       <WebView source={{ uri: 'https://google.com' }} />
