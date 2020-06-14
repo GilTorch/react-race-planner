8add998ba93767cf3a4bd3259d7cc30eea7ce6eb
dowpm <mcdowpm@gmail.com>
2020-06-13 23:10:25 -0400

    Create commentSchema for inputs error handling

--- utils/validators.js
+++ utils/validators.js
@@ -65,3 +65,7 @@ export const passwordResetVerificationSchema = yup.object().shape({
     .required('Confirm password')
     .oneOf([yup.ref('newPassword'), null], 'Passwords are not the same')
 });
+
+export const commentSchema = yup.object().shape({
+  content: yup.string().required('Please enter the comment')
+});
