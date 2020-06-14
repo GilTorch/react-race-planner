2538eedc144bbf73bbb5840d04dc0f77c9456d3a
dowpm <mcdowpm@gmail.com>
2020-06-13 20:36:29 -0400

    Create the actions to get stories and genres. create getAllStoriesAction and getAllGenresAction in the storyActions

--- redux/actions/StoryActions.js
+++ redux/actions/StoryActions.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import axios from '../../services/axiosService';
-import { Story } from './types';
+import { Story, Home } from './types';
 
 export const createReportAction = data => dispatch => {
   dispatch({ type: Story.EPORT_STORY_START });
@@ -10,6 +10,36 @@ export const createReportAction = data => dispatch => {
     .then(() => dispatch({ type: Story.EPORT_STORY_SUCCESS }))
     .catch(error => {
       dispatch({ type: Story.EPORT_STORY_FAILURE });
+
+      throw error.response?.data;
+    });
+};
+
+export const getAllStoriesAction = () => dispatch => {
+  dispatch({ type: Home.GET_ALL_STORIES_START });
+
+  return axios
+    .get('/documents')
+    .then(response => {
+      dispatch({ type: Home.GET_ALL_STORIES_SUCCESS, stories: response.data });
+    })
+    .catch(error => {
+      dispatch({ type: Home.GET_ALL_STORIES_FAILURE });
+
+      throw error.response?.data;
+    });
+};
+
+export const getAllGenresAction = () => dispatch => {
+  dispatch({ type: Home.GET_ALL_GENRES_START });
+
+  return axios
+    .get('/story-genres')
+    .then(response => {
+      dispatch({ type: Home.GET_ALL_GENRES_SUCCESS, genres: response.data });
+    })
+    .catch(error => {
+      dispatch({ type: Home.GET_ALL_GENRES_FAILURE });
 
       throw error.response?.data;
     });
