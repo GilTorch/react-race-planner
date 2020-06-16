f5032486be21d29a64f3160b906ba2387cead85b
Nick Rameau <r@meau.me>
2020-06-16 19:03:17 -0400

    Introduce the 'functions' utility file

---
+++ utils/functions.js
@@ -0,0 +1,24 @@
+import Constants from 'expo-constants';
+import { Platform } from 'react-native';
+import {
+  ANDROID_SERVER_URL,
+  IOS_SERVER_URL,
+  USER_AVATAR_UPLOAD_LOCATION
+} from 'react-native-dotenv';
+
+const platformServerURL = Platform.OS === 'android' ? ANDROID_SERVER_URL : IOS_SERVER_URL;
+
+export const getUserProfileUri = userPicture => {
+  if (!userPicture) {
+    return null;
+  }
+
+  if (userPicture.startsWith('http')) {
+    return userPicture;
+  }
+
+  return `${
+    Constants.isDevice ? ANDROID_SERVER_URL : platformServerURL
+    // TODO: Fix this annoying prettier/prettier warning
+    }/${USER_AVATAR_UPLOAD_LOCATION}/${userPicture}`;
+};
