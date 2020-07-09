ebab8d0a8f562a7e57800f25ec9b026a34f5de19
dowpm <mcdowpm@gmail.com>
2020-07-08 22:40:31 -0400

    Create action types and skipRoundAction

--- redux/actions/StoryAction.js
+++ redux/actions/StoryAction.js
@@ -1,52 +1,51 @@
-/* eslint-disable no-underscore-dangle */
 import axios from '../../services/axiosService';
 import { Story } from './types';
 
-export const joinStoryAction = (storyId, userId, privacyStatus) => dispatch => {
+export const joinStoryAction = (storyId, userId, privacyStatus) => (dispatch) => {
   dispatch({ type: Story.JOIN_STORY_START });
 
   return axios
     .post(`/documents/${storyId}/authors/${userId}`, { privacyStatus })
-    .then(response => dispatch({ type: Story.JOIN_STORY_SUCCESS, story: response.data.story }))
-    .catch(error => {
+    .then((response) => dispatch({ type: Story.JOIN_STORY_SUCCESS, story: response.data.story }))
+    .catch((error) => {
       dispatch({ type: Story.JOIN_STORY_FAILURE });
 
       throw error.response?.data;
     });
 };
 
-export const createStoryAction = data => dispatch => {
+export const createStoryAction = (data) => (dispatch) => {
   dispatch({ type: Story.CREATE_STORY_START });
 
   return axios
     .post('/documents', data)
-    .then(response => {
+    .then((response) => {
       dispatch({ type: Story.CREATE_STORY_SUCCESS, story: response.data.story });
       return response.data;
     })
-    .catch(error => {
+    .catch((error) => {
       dispatch({ type: Story.CREATE_STORY_FAILURE });
 
       throw error.response?.data;
     });
 };
 
-export const createRoundAction = (data, storyId) => dispatch => {
+export const createRoundAction = (data, storyId) => (dispatch) => {
   dispatch({ type: Story.CREATE_ROUND_START });
 
   return axios
     .post(`/documents/${storyId}/document-parts`, data)
-    .then(response => {
+    .then((response) => {
       dispatch({ type: Story.CREATE_ROUND_SUCCESS, story: response.data.story });
     })
-    .catch(error => {
+    .catch((error) => {
       dispatch({ type: Story.CREATE_ROUND_FAILURE });
 
       throw error.response?.data;
     });
 };
 
-export const leaveStoryAction = (storyId, userId) => dispatch => {
+export const leaveStoryAction = (storyId, userId) => (dispatch) => {
   dispatch({ type: Story.LEAVE_STORY_START });
 
   // Delete all parts for this author
@@ -55,14 +54,14 @@ export const leaveStoryAction = (storyId, userId) => dispatch => {
     .then(() => {
       dispatch({ type: Story.LEAVE_STORY_SUCCESS });
     })
-    .catch(error => {
+    .catch((error) => {
       dispatch({ type: Story.LEAVE_STORY_FAILURE });
 
       throw error.response?.data;
     });
 };
 
-export const deleteStoryAction = storyId => dispatch => {
+export const deleteStoryAction = (storyId) => (dispatch) => {
   dispatch({ type: Story.DELETE_STORY_START });
 
   return axios
@@ -70,8 +69,23 @@ export const deleteStoryAction = storyId => dispatch => {
     .then(() => {
       dispatch({ type: Story.DELETE_STORY_SUCCESS });
     })
-    .catch(error => {
+    .catch((error) => {
       dispatch({ type: Story.DELETE_STORY_FAILURE });
+
+      throw error.response?.data;
+    });
+};
+
+export const skipRoundAction = (documentPartId) => (dispatch) => {
+  dispatch({ type: Story.SKIP_ROUND_START });
+
+  return axios
+    .post('/documentPart', documentPartId)
+    .then(() => {
+      dispatch({ type: Story.SKIP_ROUND_SUCESS });
+    })
+    .catch((error) => {
+      dispatch({ type: Story.SKIP_ROUND_FAILURE });
 
       throw error.response?.data;
     });
