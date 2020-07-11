2021e9da50ffa884f18b57b53ea68e2ab4c48365
Nick Rameau <r@meau.me>
2020-07-10 20:19:27 -0400

    Voting: Handle case when story is still in the `home` reducer

--- redux/reducers/home.js
+++ redux/reducers/home.js
@@ -92,7 +92,10 @@ const homeReducer = (state = initialState, action) => {
     case Story.JOIN_STORY_SUCCESS:
       return {
         ...state,
-        stories: state.stories.map((s) => {
+    case Story.ROUND_VOTE_SUCCESS:
+      return {
+        ...state,
+        stories: state.stories?.map((s) => {
           // eslint-disable-next-line no-underscore-dangle
           if (s._id === action.story._id) {
             return action.story;
