aa3f27aa85e96a84544ce429a87ea38431ab7765
CornetS28 <corsam28@gmail.com>
2020-08-25 13:13:53 -0400

    delete redundant component

--- components/modals/index.js
+++ components/modals/index.js
@@ -1,5 +1,4 @@
 import CommentModal from './CommentModal';
 import VotingModal from './VotingModal';
-import TermsAndConditionsModal from './TermsAndConditionsModal';
 
-export { CommentModal, VotingModal, TermsAndConditionsModal };
+export { CommentModal, VotingModal };
