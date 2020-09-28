c9a97b2afa546eff7c29bd29877190a2dad20568
Rudy Ravelin <rudy@noukod.com>
2020-09-28 16:17:36 -0400

    Replace expo-jwt by react-native-pure-jwt

--- services/axiosService.js
+++ services/axiosService.js
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native';
 import axios from 'axios';
-import jwt from 'expo-jwt';
+import jwt from 'react-native-pure-jwt';
 import { ANDROID_BASE_URL, IOS_BASE_URL, JWT_SECRET, RERUM_KEY } from 'react-native-dotenv';
 import Constants from 'expo-constants';
 
@@ -40,22 +40,22 @@ axiosService.interceptors.response.use(
   async (response) => {
     if (response.data?.token) {
       // 1. decode the token
-      const decodedUser = jwt.decode(response.data.token, JWT_SECRET);
+      const decodedUser = await jwt.decode(response.data.token, JWT_SECRET);
 
       decodedUser.isPasswordReset = response.data.isPasswordReset;
 
       // 2. save the user and the token to the state
       store.dispatch({
         type: Auth.ADD_SESSION,
-        data: { user: decodedUser, token: response.data.token },
+        data: { user: decodedUser.payload, token: response.data.token },
       });
     }
     return response;
   },
-  (error) => {
+  async (error) => {
     if (error.response?.data?.token) {
       // 1. decode the token
-      const decodedUser = jwt.decode(error.response.data.token, JWT_SECRET);
+      const decodedUser = await jwt.decode(error.response.data.token, JWT_SECRET);
 
       decodedUser.isPasswordReset = error.response.data.isPasswordReset;
 
@@ -63,7 +63,7 @@ axiosService.interceptors.response.use(
       store.dispatch({
         type: Auth.ADD_SESSION,
         data: {
-          user: decodedUser,
+          user: decodedUser.payload,
           token: error.response.data.token,
         },
       });
