8258cd0e647e84cd06301583eac1f052528cf8fe
Holy-Elie Scaide <skydhash@gmail.com>
2020-10-21 21:20:46 -0400

    update android configuration

--- android/app/src/main/java/com/noukod/scriptorerum/generated/BasePackageList.java
+++ android/app/src/main/java/com/noukod/scriptorerum/generated/BasePackageList.java
@@ -23,7 +23,6 @@ public class BasePackageList {
         new expo.modules.location.LocationPackage(),
         new expo.modules.notifications.NotificationsPackage(),
         new expo.modules.permissions.PermissionsPackage(),
-        new expo.modules.random.RandomPackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.sqlite.SQLitePackage(),
         new expo.modules.updates.UpdatesPackage(),
